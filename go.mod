08367f3304365f2b85ba27fad81ef850b2dd9d14
Dimitris Karakasilis <dimitris@karakasilis.me>
2022-12-06 16:49:22 +0200

    Add more replacements
    and bump unstructured
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- go.mod
+++ go.mod
@@ -2,23 +2,27 @@ module github.com/spectrocloud/kubesplit
 
 go 1.18
 
+replace github.com/kairos-io/kairos => /home/dimitris/workspace/kairos/kairos
+
 require (
-	github.com/onsi/ginkgo/v2 v2.1.6
-	github.com/onsi/gomega v1.20.2
-	github.com/vmware-labs/go-yaml-edit v0.3.0
-	github.com/vmware-labs/yaml-jsonpointer v0.1.0
-	golang.org/x/text v0.3.7
+	github.com/kairos-io/kairos v1.3.0
+	github.com/onsi/ginkgo/v2 v2.5.1
+	github.com/onsi/gomega v1.24.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/go-openapi/jsonpointer v0.19.3 // indirect
-	github.com/go-openapi/swag v0.19.5 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/itchyny/gojq v0.12.10 // indirect
+	github.com/itchyny/timefmt-go v0.1.5 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
 
 replace gopkg.in/yaml.v3 => github.com/atomatt/yaml v0.0.0-20200228174225-55c5cf55e3ee
--- internal/helm/mutator.go
+++ internal/helm/mutator.go
@@ -34,6 +34,10 @@ var helmRules = []rules{
 		Replacement: replaceKeyValue(`.metadata.name`, `"{{ include \"helm-chart.fullname\" . }}"`),
 	},
 	{
+		Condition:   hasKeyWithValue(".kind", "Service"),
+		Replacement: replaceKeyValue(`.spec.selector`, `"{{- include \"helm-chart.selectorLabels\" . | nindent 6 }}"`),
+	},
+	{
 		Condition:   hasKeyWithValue(".kind", "Deployment"),
 		Replacement: replaceKeyValue(`.spec.template.spec.containers[1].image`, `"{{ .Values.image.repository | default \"\" }}:{{ .Values.image.tag | default .Chart.AppVersion }}"`),
 	},
@@ -75,12 +79,24 @@ var helmRules = []rules{
 	},
 	{
 		Condition:   hasKeyWithValue(".kind", "Deployment"),
-		Replacement: replaceKeyValue(`.spec.selector.matchLabels`, `"{{- include \"helm-chart.selectorLabels\" . | nindent 6 }}"`),
+		Replacement: replaceKeyValue(`.spec.labels`, `"{{- include \"helm-chart.labels\" . | nindent 8 }}"`),
 	},
-	// {
-	// 	Condition:   hasKeyWithValue("/kind", "Deployment"),
-	// 	Replacement: replaceKeyValue(`/spec/template/metadata/labels`, `{{- include "helm-chart.selectorLabels" . | nindent 10 }}`),
-	// },
+	{
+		Condition:   hasKeyWithValue(".kind", "Deployment"),
+		Replacement: replaceKeyValue(`.spec.selector.matchLabels`, `"{{- include \"helm-chart.selectorLabels\" . | nindent 10 }}"`),
+	},
+	{
+		Condition:   hasKeyWithValue(".kind", "Deployment"),
+		Replacement: replaceKeyValue(`.spec.template.metadata.labels`, `"{{- include \"helm-chart.selectorLabels\" . | nindent 14 }}"`),
+	},
+	{
+		Condition:   hasKeyWithValue(".kind", "Deployment"),
+		Replacement: replaceKeyValue(`.spec.template.metadata.annotations`, `"{{- range keys .Values.podAnnotations }}{{ . | quote }}: {{ get $.Values.podAnnotations . | quote}}{{- end }}"`),
+	},
+	{
+		Condition:   hasKeyWithValue(".kind", "Deployment"),
+		Replacement: replaceKeyValue(`.imagePullSecrets`, `"{{ toYaml .Values.imagePullSecrets | nindent 14 }}"`),
+	},
 }
 
 func hasKey(key string) func(content []byte, data map[string]interface{}) bool {
